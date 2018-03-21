5371c7a09bb96ad1920edeb62991c536ba983445
AndresAcosta08 <37352270+AndresAcosta08@users.noreply.github.com>
2018-03-21 20:26:04 +0000

    Update AndresAcosta_generar.py

--- AndresAcosta_generar.py
+++ AndresAcosta_generar.py
@@ -5,29 +5,30 @@ import matplotlib.pyplot as plt
 
 def sample_1(N):
 	numeros = np.random.random_sample(N)
+	
+#Retorna el resultado de la funcion 1, con N como parametro.
 	return numeros
 
-#Imprime el resultado de la funcion 1, con N como parametro.
-#print sample_1(5)
 
 #La funcion genera y retorna N numeros aleatorios siguiendo la distribucion probabilidad exponencial con Beta= 0.5.
 def sample_2(escala, N):
 	escala=0.5
 	numeros2 = np.random.exponential(escala,N)
+	
+#Retorna el resultado de la funcion 2, con Escala y N como parametro.
 	return numeros2
 
-#Imprime el resultado de la funcion 2, con N como parametro.
-#print sample_2(0.5,5)
 
+
+#La funcion genera y retorna M promedios Sn dada la función de sampleo.
 def get_mean(sampling_fun,N,M):
 	promedios=[]
 	for i in range(N):
 		prom=sampling_fun(M)
 		promedios.append(prom)
 
+#Retorna el resultado de la funcion 3, con una funcion, N y M como parametros.
 	return promedios
-
-print get_mean(sample_1,5,10000)
 
 
 
